285df6fd5ffb455a9e6871188fa5b9136abd0ecd
안예지 <an-yeji@an-yejiui-MacBookPro.local>
2020-03-16 11:58:28 +0900

    Popup

--- STT_KIOSK/app/src/main/java/com/example/stt_kiosk/MainActivity.java
+++ STT_KIOSK/app/src/main/java/com/example/stt_kiosk/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.stt_kiosk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -198,75 +198,111 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()){
             case R.id.menu_btn1:
                 name = (TextView) findViewById(R.id.menu_name1);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent1.putExtra("data", name.getText().toString());
+                startActivityForResult(intent1, 1);
                 break;
             case R.id.menu_btn2:
                 name = (TextView) findViewById(R.id.menu_name2);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent2.putExtra("data", name.getText().toString());
+                startActivityForResult(intent2, 1);
                 break;
             case R.id.menu_btn3:
                 name = (TextView) findViewById(R.id.menu_name3);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent3 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent3.putExtra("data", name.getText().toString());
+                startActivityForResult(intent3, 1);
                 break;
             case R.id.menu_btn4:
                 name = (TextView) findViewById(R.id.menu_name4);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent4 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent4.putExtra("data", name.getText().toString());
+                startActivityForResult(intent4, 1);
                 break;
             case R.id.menu_btn5:
                 name = (TextView) findViewById(R.id.menu_name5);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent5 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent5.putExtra("data", name.getText().toString());
+                startActivityForResult(intent5, 1);
                 break;
             case R.id.menu_btn6:
                 name = (TextView) findViewById(R.id.menu_name6);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent6 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent6.putExtra("data", name.getText().toString());
+                startActivityForResult(intent6, 1);
                 break;
             case R.id.menu_btn7:
                 name = (TextView) findViewById(R.id.menu_name7);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent7 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent7.putExtra("data", name.getText().toString());
+                startActivityForResult(intent7, 1);
                 break;
             case R.id.menu_btn8:
                 name = (TextView) findViewById(R.id.menu_name8);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent8 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent8.putExtra("data", name.getText().toString());
+                startActivityForResult(intent8, 1);
                 break;
             case R.id.menu_btn9:
                 name = (TextView) findViewById(R.id.menu_name9);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent9 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent9.putExtra("data", name.getText().toString());
+                startActivityForResult(intent9, 1);
                 break;
             case R.id.menu_btn11:
                 name = (TextView) findViewById(R.id.menu_name11);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent11 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent11.putExtra("data", name.getText().toString());
+                startActivityForResult(intent11, 1);
                 break;
             case R.id.menu_btn12:
                 name = (TextView) findViewById(R.id.menu_name12);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent12 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent12.putExtra("data", name.getText().toString());
+                startActivityForResult(intent12, 1);
                 break;
             case R.id.menu_btn13:
                 name = (TextView) findViewById(R.id.menu_name13);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent13 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent13.putExtra("data", name.getText().toString());
+                startActivityForResult(intent13, 1);
                 break;
             case R.id.menu_btn14:
                 name = (TextView) findViewById(R.id.menu_name14);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent14 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent14.putExtra("data", name.getText().toString());
+                startActivityForResult(intent14, 1);
                 break;
             case R.id.menu_btn15:
                 name = (TextView) findViewById(R.id.menu_name15);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent15 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent15.putExtra("data", name.getText().toString());
+                startActivityForResult(intent15, 1);
                 break;
             case R.id.menu_btn16:
                 name = (TextView) findViewById(R.id.menu_name16);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent16 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent16.putExtra("data", name.getText().toString());
+                startActivityForResult(intent16, 1);
                 break;
             case R.id.menu_btn17:
                 name = (TextView) findViewById(R.id.menu_name17);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent17 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent17.putExtra("data", name.getText().toString());
+                startActivityForResult(intent17, 1);
                 break;
             case R.id.menu_btn18:
                 name = (TextView) findViewById(R.id.menu_name18);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent18 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent18.putExtra("data", name.getText().toString());
+                startActivityForResult(intent18, 1);
                 break;
             case R.id.menu_btn19:
                 name = (TextView) findViewById(R.id.menu_name19);
-                Toast.makeText(this.getApplicationContext(),name.getText().toString(),Toast.LENGTH_SHORT).show();
+                Intent intent19 = new Intent(this, com.example.stt_kiosk.PopupActivity.class);
+                intent19.putExtra("data", name.getText().toString());
+                startActivityForResult(intent19, 1);
                 break;
         }
     }
